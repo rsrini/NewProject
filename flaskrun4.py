c0efb22bf63090439312dbe79f50f8c594dd4a82
Srinivasan <rs.vasu@gmail.com>
2020-07-27 21:53:30 +0530

    Flask related files

---
+++ flaskrun4.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+app = Flask(import_name=__name__)
+@app.route("/echo", methods=["POST"])
+def echo():
+ name = request.form.get("name", "")
+ age = request.form.get("age", "")
+ response = "Hey there {}! You said you are {} years old.".format(name, age)
+ return response
+app.run()
